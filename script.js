56693ea5eb028bd6420abce75a8fe6c06c30a381
Zippy-boy <zippy-boy@outlook.com>
2022-10-04 20:14:17 +0100

    a

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-
-
-function randomWord() {
+// get a random word from internet and the definition of that word
+function getWord() {
     var request = new XMLHttpRequest();
     request.open('GET', 'https://random-word-api.herokuapp.com/word?number=1', true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
-            console.log(data)
             document.getElementById("word-of-day").innerHTML = data[0];
-            showDef();
         } else {
             console.log('error');
         }
@@ -16,13 +13,16 @@ function randomWord() {
     request.send();
 }
 
-showDef = () => {
+getWord();
+
+
+
+function getDef() {
     var request = new XMLHttpRequest();
     request.open('GET', 'https://api.dictionaryapi.dev/api/v2/entries/en/' + document.getElementById("word-of-day").innerHTML, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
             var data = JSON.parse(request.responseText);
-            console.log(data)
             document.getElementById("def").innerHTML = data[0].meanings[0].definitions[0].definition;
         } else {
             console.log('error');
@@ -30,7 +30,3 @@ showDef = () => {
     };
     request.send();
 }
-
-
-
-randomWord();
