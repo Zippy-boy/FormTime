5ce9007edeb5e6cf4f36f1d657fe273ed76536f1
Zippy-boy <zippy-boy@outlook.com>
2022-10-12 19:25:06 +0100

    Create brainteaser.py

---
+++ help-python/brainteaser.py
@@ -0,0 +1,217 @@
+a = """
+1. Q: Before Mt. Everest was discovered, which was the highest mountain in
+the world?
+A: Mt. Everest. It just wasn't discovered yet.
+
+2. Q: If you were running a race and you passed the person in 2nd place, what
+place would you be in now?
+A: Second place.
+
+3.  Q: If a red house is made of red bricks, a blue house is made of blue
+bricks, and a pink house is made of pink bricks, what is a green house made
+of?
+A: Glass.
+
+4. Q: If you throw a red stone into the blue sea, what it will become?
+A: Wet.
+
+5. Q: How many months have 28 days?
+A: All of them.
+
+6. Q: What is full of holes but still holds water?
+A: A sponge.
+
+7. Q: What is always in front of you but can't be seen?
+A: The future.
+
+3. Q: What can run but never walks, has a mouth but never talks, has a head
+but never weeps, and has a bed but never sleeps?
+A: A river.
+4. Q: Imagine you are in a sinking rowboat surrounded by sharks. How would
+you survive?
+A: Stop imagining.
+5. Q: What is as big as you are and yet does not weigh anything?
+A: Your shadow.
+6. Q: Two cannibals were chatting as they had their dinner. One complained
+that he really quite disliked his new mother-in-law. What was the advice given
+to him by his companion?
+A: So just finish your vegetables!
+7. Q: Paul's height is six feet, he's an assistant at a butcher's shop, and wears
+size 9 shoes. What does he weigh?
+A: Meat.
+8. Q: What's black and white and red all over?
+A: An embarrassed zebra.
+9. Q: I can run but I can't walk, a mouth but I can't talk, a head but I can't
+think, a bed but I can't sleep. Who am I?
+A: River.
+10. Q: I'm where yesterday follows today, and tomorrow's in the middle. What
+am I?
+A: A dictionary.
+
+11. Q: Eskimos are very good hunters, but they never hunt penguins. Why
+not?
+A: Because Eskimos live at the north pole, and penguins live in the south pole.
+12. Q: It goes in dry, it comes out wet, the longer it's in the stronger it gets.
+What is it?
+A: Tea-bag
+13. Q: The peacock is a bird that does not lay eggs. How do they get baby
+peacocks?
+A: The peahen lays eggs.
+14. Q: Where do fish keep their money?
+A: The River Bank
+15. Q: Which is heavier a ton of gold or a ton of silver?
+A: Both are equal. 1 ton = 1 ton
+16. Q: What did one eye say to the other?
+A: Between us something smells.
+17. Q: What was the President's name in 1975?
+A: The same as it is today.
+18. Q: There are 6 mangoes in a basket and 6 kids eagerly waiting to get one.
+Each kid is given 1 mango, yet there is one mango in the basket, how?
+A: The last kid gets the basket with the mango in it.
+19. Q: 2 fathers and 2 sons go fishing. Each of them catches one fish. So why
+do they bring home only 3 fish?
+A: Because the fishing group comprises a grandfather, his son, and his
+grandson - hence, just three people.
+
+20. Q: You can hold it without using your hands or arms. What is it?
+A: Your breath.
+21. Q: I stand when I'm sitting, and jump when I'm walking. Who am I?
+A: A kangaroo.
+22. Q: A certain five letter word becomes shorter when you add two letters to
+it. What is the word?
+A: Short.
+23. Q: How long is the answer to this question.
+A: How long.
+24. Q: How many seconds are there in a year?
+A: Twelve. The Second of Jan, Feb, March...December.
+25. Q: Is it legal for a man to marry his widow's sister?
+A: No. He is dead.
+26. Q: Why is it against the law for a person living in New York to be buried in
+California?
+A: He is alive
+27. Q: What goes up but never comes down?
+A: Your age.
+28. Q: You throw away the outside and cook the inside. Then you eat the
+outside and throw away the inside. What did you eat?
+A: An ear of corn.
+29. Q: What travels around the world but stays in one spot?
+A: A stamp!
+
+30. Q: What do the letter 't' and an island have in common?
+A: Both are in water — WaTer.
+31. Q: I go all around the world but I still stay in a corner. Who am I?
+A: A postage stamp.
+32. Q: Which month has 28 days?
+: All
+33. Q: What is in the middle of CHINA?
+A: The letter
+34. Q: What clothes did Delaware?
+A: Don't know. Alaska! (I'll ask her).
+35. Q: When you have me, you feel like sharing me. But, if you do share me,
+you don't have me. What am I?
+A: A secret.
+36. Q: A cowboy rode into town on Friday, stayed three days, and rode out
+again on Friday. How did he do that?
+A: His horse's name is Friday.
+37. Q: The person who makes it has no need for it. The person who purchases
+it does not use it. The person who does use it does not know he or she is.
+What is it?
+A: A coffin.
+38. Q: It is an insect, and the first part of its name is the name of another
+insect. What is it?
+A: Beetle.
+
+39. Q: Divide 50 by half and add 20.
+A: 120
+40. Q: 20 pigeons sat on the branches of a tree. A man shot 1 pigeon with his
+gun. How many were left on the tree?
+A: None. The rest flew away.
+41. Q: If an electric train is traveling south, which way is the smoke going?
+A: There is no smoke, it's an electric train!
+42. Q: What's black when you get it, red when you use it, and white when
+you're all through with it?
+A: Charcoal.
+43. Q: Heavy it is but reverse it's not?
+A: TON
+44. Q: When was it that Christmas and New Year were celebrated in the same
+year?
+A: Every year.
+45. Q: How many time can you subtract 10 from 100?
+A: Once. Next time you would be subtracting 10 from 90.
+46. Q: Adam's mother had three children. The first child was named April. The
+second child was named May. What was the third child's name?
+A: Adam.
+47. Q: A clerk at a butcher shop stands five-feet ten-inches tall and wears size
+13 sneakers. What does he weigh?
+A: Meat.
+
+48. Q: What types of words are these: madam, civic, eye, level?
+A: They are palindromes; they read the same both ways.
+49. Q: What ends everything always?
+A: The alphabet
+50. Q: Feed me and I live, give me drink and I die. What am I?
+A: Fire. If you keep adding fuel (feeding it) to the fire, it will keep burning.
+However, a little water will extinguish or kill it.
+51. Q: What are the next two letters in this series? J F M A M J J A S O
+A: N and D. These are initials of the months in a year.
+52. Q: Which came first, the chicken or the egg?
+A: The egg, since dinosaurs laid eggs long before there were chickens.
+53. Q: Before Mount Everest was discovered, what was the highest mountain
+on Earth?
+A: Mount Everest, of course! Irrespective of the discovery, Mount Everest
+remains the highest peak in the world.
+54. Q: While some months have just 30 days, others have 31 days. How many
+months have 28 days?
+A: Every month has 28 days.
+55. Q: What are two things you cannot have for breakfast?
+A: Lunch and dinner! One can only eat breakfast as an early morning meal;
+the rest would be called lunch and dinner. So, technically, one cannot have
+lunch or dinner for breakfast.
+
+56. Q: What is broken every time it's spoken?
+A: Silence. We often use the phrase 'to break a silence' when me mean there
+is noise or sound. Thus, every time someone speaks, we figuratively break the
+silence.
+57. Q: If I give you a group of three, one who is sitting down and will never
+get up. The second, who eats as much as is given to him, yet is always
+hungry. The third, who goes away and never returns. What are we?
+A: Stove, fire, smoke. Here 'stove' represents the one who is always sitting,
+'fire' which eats or consumes everything and is yet hungry for more, and
+finally 'smoke' which gets flown away with air, only never to return.
+58. Q: What comes once in a minute, twice in a moment, but never in a
+thousand years?
+A: The letter M. The riddle questions the number of times the letter 1m'
+appears in the words, 'minute', 'moment', and 'years'.
+59. Q: What has a mouth but can't chew?
+A: River. It has a mouth, but it definitely cannot chew anything!
+60. Q: What is as big as an elephant but weighs nothing at all?
+A: An elephant's shadow!
+61. Q: I go through towns and hills but I never move. Who am I?
+A: Road. A road goes through most of the places, but never itself moves.
+
+62. Q: The more you take, the more you leave behind.
+A: Footsteps. The more you walk, the more traces you leave behind.
+63. Q: What did the fish say when he ran into a concrete wall?
+A: Damn! The poor fish banged into the wall.
+64. Q: What starts with a T, ends with a T, and has T in it?
+A: Teapot.
+65. Q: What English word retains the same pronunciation, even after you take
+away four of its five letters?
+A: Queue. When we remove mueue' from the word 'Queue', it still retains the
+sound of the letter 'Q', which is same as the word 'Queue'.
+66. Q: If there are three cups of sugar and you take one away, how many do
+you have?
+A: One, as that is the only one you took away and that is all that you have.
+The rest still remain where there were.
+67. Q: How many letters are in the alphabet?
+A: We meant the word 'alphabet' and not the series of alphabets. So, the right
+answer is 8.
+68. Q: Take away the whole and some still remains. What is it?
+A: The word 'wholesome.' If you remove the 'whole' or the first part of the
+word, 'some' will still remain as an independent word.
+69. Q: How do you double your money in the easiest and fastest way?
+A: Place it in front of the mirror! The reflection will make it look like there is
+twice as much as actual money.
+
+
+"""
