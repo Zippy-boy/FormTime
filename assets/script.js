45acd7cc098c39a9100acecc4d904e688a4d4dec
Zippy-boy <zippy-boy@outlook.com>
2022-11-05 15:30:00 +0000

    asd

--- assets/script.js
+++ assets/script.js
@@ -131,7 +131,7 @@ fetch(`https://api.wikimedia.org/feed/v1/wikipedia/en/onthisday/all/${month}/${d
 // Definatly not clean https://v2.jokeapi.dev/joke/Any
 
 const newJoke = () => {
-    fetch("https://v2.jokeapi.dev/joke/Programming,Miscellaneous,Pun,Spooky,Christmas?blacklistFlags=nsfw,religious,racist,sexist,explicit")
+    fetch("https://v2.jokeapi.dev/joke/Any")
         .then(response => response.json())
         .then(data => {
             let joke = data.joke;
